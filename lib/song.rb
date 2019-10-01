4475a771c39be1dfb854a640de0cc2ac94331117
Mark Stone <markwstone85@gmail.com>
2019-10-01 22:35:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,10 +23,11 @@ class Song
   end
 
   def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    @@all << song
-    song
+    self.new_by_name(name)
+    # song = self.new
+    # song.name = name
+    # @@all << song
+    # song
   end
 
   def self.find_by_name(name)
